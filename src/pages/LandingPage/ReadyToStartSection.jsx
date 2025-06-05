cf53037eb5fb8a812d1e4c7158bd7d6d944400a5
Priya Rathore <199036210+priyarathore17@users.noreply.github.com>
2025-06-05 10:10:20 +0530

    Update ReadyToStartSection.jsx

--- src/pages/LandingPage/ReadyToStartSection.jsx
+++ src/pages/LandingPage/ReadyToStartSection.jsx
@@ -45,6 +45,7 @@ const ReadyToStartSection = () => {
           //   spacing={theme.spacing(6)}
           alignItems="center"
           justifyContent={'center'}
+          gap={4}
           sx={{
             height: '100%',
             flexDirection: { xs: 'column', md: 'row' },
